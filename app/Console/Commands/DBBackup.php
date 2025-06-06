8b566cc428166854b1606d3721f90bfba409edec
dragor.dev <dragor.dev@gmail.com>
2025-06-06 13:46:31 +0200

    Finished 008-added-backup-db

---
+++ app/Console/Commands/DBBackup.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Process;
+use Illuminate\Support\Facades\Storage;
+
+class DBBackup extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:dbbackup';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        // Backup Not Working
+
+//        $path = Storage::path("/backup/".now()->format("d-m-Y-H-i-s").".gz");
+//
+//        $command = "mysqldump --user=".env('DB_USERNAME')." --password=".env('DB_PASSWORD')." --host=". env('DB_HOST')." ".env('DB_DATABASE'). " | gzip > ". $path;
+//
+//        $process = Process::run($command);
+//
+//        if ($process->successful()) {
+//            $this->info('Database backup saved as:' . basename($path));
+//        }
+
+    }
+}
